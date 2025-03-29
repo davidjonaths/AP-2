1d9dde682c3247a92bd11bb11e958a5aaa5a4a03
davidjonaths <davidjonaths@gmail.com>
2025-03-29 17:16:19 +0700

    Pertemuan 4 : Perulangan

---
+++ Pertemuan 4/Latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string kalimat;
+    int i;
+
+    system("CLS");
+
+    cout << "Masukkan kalimat : ";
+    getline (cin, kalimat);
+
+    // a l y a 
+    for(i = 0; i < kalimat.length(); i++) {
+        kalimat[i] = toupper(kalimat[i]);
+    }
+
+    cout << "Kalimat dalam huruf kapital : " << kalimat << endl;
+
+    return 0;
+}
