51b6448cf0421b2328ad12aa92820525d2570e24
davidjonaths <davidjonaths@gmail.com>
2025-05-25 12:49:15 +0700

    Update classLevel.cpp

--- Pertemuan 8/classLevel.cpp
+++ Pertemuan 8/classLevel.cpp
@@ -4,51 +4,54 @@
 using namespace std;
 
 class ContohAkses {
-    private:
-       int privateVar;
-    
-    protected:
-       int protectedVar;
+private:
+    int privateVar;    // hanya bisa diakses dalam class ini saja
 
-    public:
-       int publicVar;
+protected:
+    int protectedVar;  // bisa diakses dalam class ini dan turunannya
 
-       // constructor
-       ContohAkses() {
+public:
+    int publicVar;     // bisa diakses dari mana saja
+
+    // constructor
+    ContohAkses() {
         privateVar = 1;
         protectedVar = 2;
         publicVar = 3;
-       }
-      
+    }
 
     void tampilkanSemua() {
         cout << "Akses dari dalam class : " << endl;
-        cout << privateVar << endl;
-        cout << protectedVar << endl;
-        cout << publicVar << endl;
+        cout << "privateVar = " << privateVar << endl;
+        cout << "protectedVar = " << protectedVar << endl;
+        cout << "publicVar = " << publicVar << endl;
     }
 };
 
 // kelas turunan
 class turunan : public ContohAkses {
-    public:
+public:
     void aksesProtected() {
-        cout << "akses publicVar" << publicVar << endl;
-        cout << "akses protectedVar" << protectedVar << endl;
+        cout << "akses publicVar: " << publicVar << endl;
+        cout << "akses protectedVar: " << protectedVar << endl;
+        // cout << "akses privateVar: " << privateVar << endl; // ERROR, tidak bisa akses privateVar
     }
 };
 
-int main () {
-    system ("cls");
+int main() {
+    system("cls");
 
     ContohAkses obj;
     obj.tampilkanSemua();
 
     cout << "Akses dari luar class : " << endl;
-    cout << obj.publicVar << endl;
-    // cout << obj.protectedVar << endl; //erorrrrrrrrrrrrrrrr
-    // cout << obj.privateVar << endl; //erorrrrrrrrrrrrrr
+    cout << "publicVar = " << obj.publicVar << endl;
+    // cout << obj.protectedVar << endl; // ERROR, tidak bisa akses protected dari luar class/turunan
+    // cout << obj.privateVar << endl;   // ERROR, tidak bisa akses private dari luar class
 
-      tur;
+    turunan tur;
     tur.aksesProtected();
+
+    return 0;
 }
+
