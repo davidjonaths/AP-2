70a21588e72bc3ece4be00bdc22c15af2d2a34c3
davidjonaths <davidjonaths@gmail.com>
2025-05-25 12:45:12 +0700

    Update latihanArray.cpp

--- Pertemuan 5/latihanArray.cpp
+++ Pertemuan 5/latihanArray.cpp
@@ -9,25 +9,29 @@ int main() {
     int hasil[2][2];
 
     cout << "Matriks 1" << endl;
-    for(int i = 0; i < 2; j++)
+    for(int i = 0; i < 2; i++) {           // perbaikan: i++, bukan j++
         for(int j = 0; j < 2; j++) {
             cout << "Masukkan elemen baris " << i + 1 << " kolom " << j + 1 << " : ";
             cin >> matriks1[i][j];
         }
     }
-    cout << "Matriks2" << endl;
-    for(int i = 0; i < 2; j++) {
+
+    cout << "Matriks 2" << endl;
+    for(int i = 0; i < 2; i++) {          // perbaikan: i++, bukan j++
         for(int j = 0; j < 2; j++) {
             cout << "Masukkan elemen baris " << i + 1 << " kolom " << j + 1 << " : ";
             cin >> matriks2[i][j];
         }
     }
 
-    cout << "Hasil Penjumlahan " << endl;
+    cout << "Hasil Penjumlahan Matriks:" << endl;
     for(int i=0; i<2; i++) {
         for(int j=0; j<2; j++) {
-            hasil[i][j] = matriks[i][j] + matriks2[i][j];
+            hasil[i][j] = matriks1[i][j] + matriks2[i][j];  // perbaikan variabel matriks1 bukan matriks
             cout << hasil[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
+
+    return 0;
 }
