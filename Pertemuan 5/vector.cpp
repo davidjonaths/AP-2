eb5aeedbc3b9f9ddddb3c8e4f90b1231d358a5f9
davidjonaths <davidjonaths@gmail.com>
2025-05-25 12:46:03 +0700

    Update vector.cpp

--- Pertemuan 5/vector.cpp
+++ Pertemuan 5/vector.cpp
@@ -4,32 +4,32 @@
 using namespace std;
 
 int main (){
-// Vector Declaration & Initialization
+    // Deklarasi dan inisialisasi vector bertipe string dengan beberapa nama karyawan
     vector<string> nama_karyawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"};
 
-// Ascending Element of Vector
+    // Menampilkan isi vector dengan menggunakan loop biasa (index-based)
     // for(int i=0; i< nama_karyawan.size(); i++) {
     //     cout << nama_karyawan[i] << endl;
     // }
 
+    // Menampilkan isi vector menggunakan range-based for loop (lebih simpel)
     // for (string karyawan : nama_karyawan) {
     //     cout << karyawan << endl;
     // }
 
-// Add data to vector 
+    // Menambahkan elemen baru di akhir vector dengan push_back()
     nama_karyawan.push_back("Imam");
 
-    // for(int i=0; i< nama_karyawan.size(); i++) {
-    //     cout << nama_karyawan[i] << endl;
-    // }
+    // Menampilkan vector setelah penambahan elemen (bisa dicoba dengan loop di atas)
 
-// Delete data from vector
+    // Menghapus elemen terakhir dari vector menggunakan pop_back()
     nama_karyawan.pop_back();
+
+    // Menghapus elemen pada posisi index ke-3 (elemen ke-4) menggunakan erase()
     nama_karyawan.erase(nama_karyawan.begin() + 3);
 
+    // Menampilkan isi vector setelah penghapusan elemen
     for(int i=0; i< nama_karyawan.size(); i++) {
         cout << nama_karyawan[i] << endl;
     }
-
-
 }
