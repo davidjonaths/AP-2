a9bb28277b2a5dfa06a81db02941a4a42a0647e0
davidjonaths <davidjonaths@gmail.com>
2025-05-25 12:48:51 +0700

    Update aksesStruct.cpp

--- Pertemuan 8/aksesStruct.cpp
+++ Pertemuan 8/aksesStruct.cpp
@@ -2,6 +2,7 @@
 #include <string>
 using namespace std;
 
+// Mendefinisikan struct mahasiswa dengan atribut nama, umur, dan ipk
 struct mahasiswa {
     string nama;
     int umur;
@@ -9,19 +10,27 @@ struct mahasiswa {
 };
 
 int main() {
-
+    // Membuat objek mahasiswa mhs1
     mahasiswa mhs1;
 
+    // Mengisi data mhs1
     mhs1.nama = "Dave";
     mhs1.umur = 20;
     mhs1.ipk = 4.00;
 
+    // Cara akses langsung menggunakan objek
     // cout << "Nama = " << mhs1.nama << endl;
     // cout << "Umur = " << mhs1.umur << endl;
     // cout << "IPK = " << mhs1.ipk << endl;
 
+    // Membuat pointer yang menunjuk ke mhs1
     mahasiswa *ptrmhs = &mhs1;
-    cout << "nama = " << ptrmhs -> nama << endl;
-    cout << "umur = " << ptrmhs -> umur << endl;
-    cout << "ipk = " << ptrmhs -> ipk << endl;
+
+    // Mengakses anggota struct menggunakan pointer dengan operator '->'
+    cout << "nama = " << ptrmhs->nama << endl;
+    cout << "umur = " << ptrmhs->umur << endl;
+    cout << "ipk = " << ptrmhs->ipk << endl;
+
+    return 0;
 }
+
