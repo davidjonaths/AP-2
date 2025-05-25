0715e7f65472cc38c8d43c540694df3291a1f0ee
davidjonaths <davidjonaths@gmail.com>
2025-05-25 12:24:01 +0700

    Update OperasiKondisi.cpp

--- Pertemuan 3/OperasiKondisi.cpp
+++ Pertemuan 3/OperasiKondisi.cpp
@@ -4,95 +4,104 @@ using namespace std;
 int main () {
     int nilai;
 
-    system("CLS");
+    system("CLS"); // membersihkan layar (Windows only)
 
     cout << "Masukkan nilai : ";
-    cin >> nilai;
+    cin >> nilai;  // input nilai dari user
 
-    // if statement 
-    // if (nilai <= 65) {
-    //     cout << "Anda tidak lulus!" << endl;
-    // }
+    // Contoh if statement sederhana
+    /*
+    if (nilai <= 65) {
+        cout << "Anda tidak lulus!" << endl;
+    }
+    */
 
-    // if - else statement
-    // if (nilai <= 65) {
-    //     cout << "Anda tidak lulus!" << endl;
-    // }else {
-    //     cout << "Anda lulus!" << endl;
-    // }
+    // Contoh if-else statement
+    /*
+    if (nilai <= 65) {
+        cout << "Anda tidak lulus!" << endl;
+    } else {
+        cout << "Anda lulus!" << endl;
+    }
+    */
 
-    // if - else - if statement 
-//     if (nilai == 100) {
-//         cout << "Anda keceh" << endl;
-//     } else if { (nilai <=65)
-//         cout << "Anda tidak lulus" << endl;
-//     } else {
-//         cout << "Anda lulus!" << endl;
-//     }
+    // Contoh if-else if-else statement (perbaikan sintaks)
+    /*
+    if (nilai == 100) {
+        cout << "Anda keceh" << endl;
+    } else if (nilai <= 65) {  // perbaikan: tanda kurung if-else if yang benar
+        cout << "Anda tidak lulus" << endl;
+    } else {
+        cout << "Anda lulus!" << endl;
+    }
+    */
 
-// }
+    // Contoh nested if
+    /*
+    if (nilai <= 65) {
+        cout << "Anda tidak lulus!" << endl;
+    } else {
+        if (nilai == 100) {
+            cout << "Anda lulus dan hebat" << endl;
+        } else {
+            cout << "Anda lulus" << endl;
+        }
+    }
+    */
 
-// nested if
-// if (nilai <= 65) {
-//     cout << "Anda tidak lulus!" << endl;
-//     } else {
-//         if (nilai == 100) {
-//         cout << "Anda lulus dan hebat" << endl;
-//      } else {
-//         cout << "Anda lulus" << endl;
-//      }
-//     }
+    // Contoh switch case untuk hari
+    /*
+    switch (nilai) {
+        case 1:
+            cout << "Senin" << endl;
+            break;
+        case 2:
+            cout << "Selasa" << endl;
+            break;
+        case 3:
+            cout << "Rabu" << endl;
+            break;
+        case 4:
+            cout << "Kamis" << endl;
+            break;
+        case 5:
+            cout << "Jumat" << endl;
+            break;
+        case 6:
+            cout << "Sabtu" << endl;
+            break;
+        case 7:
+            cout << "Minggu" << endl;
+            break;
+        default:
+            cout << "Inputan tidak valid" << endl;
+            break;
+    }
+    */
 
-// switch case
-// switch (nilai) {
-//     case 1:
-//         cout << "Senin" << endl;
-//         break;
-//     case 2:
-//         cout << "Selasa" << endl;
-//         break;
-//     case 3:
-//         cout << "Rabu" << endl;
-//         break;
-//     case 4:
-//         cout << "Kamis" << endl;
-//         break;
-//     case 5:
-//         cout << "Jumat" << endl;
-//         break;
-//     case 6:
-//         cout << "Sabtu" << endl;
-//         break;
-//     case 7:
-//         cout << "Minggu" << endl;
-//         break;
-//     default: // selain dari pernyataan yang ada
-//         cout << "Inputan tidak valid" << endl;
-//         break; // memberhentikan switch case
-//     }
-// }
+    // ***Catatan: C++ standar tidak mendukung range case seperti Swift***
+    // Contoh switch range (tidak valid di C++)
+    /*
+    switch (nilai) {
+        case 85 ... 100 : cout << "A" << endl; break; // tidak valid di C++
+        case 80 ... 84 : cout << "B+" << endl; break;
+        // dan seterusnya...
+        default: cout << "E" << endl;
+    }
+    */
 
-// swift range
-//     switch (nilai) {
-//         case 85 ... 100 : cout << "A" << endl; break;
-//         case 80 ... 84 : cout << "B+" << endl; break;
-//         case 75 ... 79 : cout << "B" << endl; break;
-//         case 70 ... 78 : cout << "C+" << endl; break;
-//         case 65 ... 69 : cout << "C" << endl; break;
-//         case 60 ... 64 : cout << "D" << endl; break;
-//         default: cout << "E" << endl;
-//     }
-// }
+    // Ternary Operator (penggunaan operator ternary benar)
+    if (nilai % 2 == 0) {
+        cout << "Genap sih" << endl;
+    } else {
+        cout << "Ganjil sih" << endl;
+    }
 
-// Ternary Operator
-if (nilai % 2 == 0) {
-    cout << "Genap sih" << endl;
-} else {
-    cout << "Ganjil sih" << endl;
-}
-
-    a > 5 ? 9 : 6
+    // Perbaikan penggunaan ternary yang kamu coba:
+    // "a > 5 ? 9 : 6" ini tidak lengkap dan tidak perlu di sini
+    // Berikut versi yang benar:
     string checkNum = (nilai % 2 == 0) ? "Genap" : "Ganjil";
     cout << nilai << " tuh Bilangan " << checkNum << " sih " << endl;
 
+    return 0;
 }
