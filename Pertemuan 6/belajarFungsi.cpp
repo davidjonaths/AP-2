e8fed3b0dac596a0bb510977491d1991065e1fd8
davidjonaths <davidjonaths@gmail.com>
2025-05-25 12:46:32 +0700

    Update belajarFungsi.cpp

--- Pertemuan 6/belajarFungsi.cpp
+++ Pertemuan 6/belajarFungsi.cpp
@@ -1,15 +1,22 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
+// Fungsi sapa dengan parameter formal berupa string nama
 void sapa(string nama){
+    // Menampilkan pesan sapaan dengan nama yang diberikan
     cout << "Halo " << nama << " Selamat Datang di AP 2 !" << endl;
 }
- //Parameter formal adalah parameter yang ada saat fungsi dibuat
- //Parameter Aktual adalah parameter yang digunakan saat dipanggil fungsi
+
+/*
+Parameter formal adalah parameter yang didefinisikan saat fungsi dibuat, dalam hal ini 'nama'
+Parameter aktual adalah nilai atau variabel yang dikirim ke fungsi saat fungsi dipanggil
+*/
 
 int main(){
+    // Mendefinisikan variabel namaPengguna dengan nilai "Dave"
     string namaPengguna = "Dave";
 
+    // Memanggil fungsi sapa dengan mengirimkan namaPengguna sebagai parameter aktual
     sapa(namaPengguna);
 
     return 0;
