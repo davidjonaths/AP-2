c20c24f5dc26af4bec454bb86975896df78926fb
davidjonaths <davidjonaths@gmail.com>
2025-05-25 12:43:44 +0700

    Update Latihan3.cpp

--- Pertemuan 4/Latihan3.cpp
+++ Pertemuan 4/Latihan3.cpp
@@ -2,24 +2,25 @@
 using namespace std;
 
 int main() {
-    string password, sandi, expected = "";
+    string password, sandi, expected = ""; // expected untuk menyimpan pola yang dibangun
 
     cout << "Masukkan kata sandi : ";
-    cin >> password;
+    cin >> password;  // input kata sandi pertama
 
     cout << "Masukkan kata : ";
-    cin >> sandi;
+    cin >> sandi;     // input kata kedua yang akan dicek pola
 
-    // Membuat string "expected" berdasarkan pola tertentu
+    // Membuat string expected berdasarkan pola bertingkat dari password
+    // Contoh: jika password = "abc", expected = "a" + "ab" + "abc" = "aababc"
     for (int i = 0; i < password.length(); i++) {
         expected += password.substr(0, i + 1);
     }
 
-    // Mengecek apakah input kedua sesuai dengan pola yang diharapkan
+    // Membandingkan apakah input kedua sesuai dengan pola yang diharapkan
     if (expected == sandi) {
-        cout << "Tulisan agen benar" << endl;
+        cout << "Tulisan agen benar" << endl; // pola cocok
     } else {
-        cout << "Tulisan agen salah" << endl;
+        cout << "Tulisan agen salah" << endl; // pola tidak cocok
     }
 
     return 0;
